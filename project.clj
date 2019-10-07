075acb9bd7ac592002e3f188330d39f2fbb2c04e
Hugo Blanco <blancohfb@gmail.com>
2019-10-07 16:35:27 -0300

    First version

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
-(defproject blancohugo.luhn "0.1.0-SNAPSHOT"
+(defproject blancohugo.luhn "1.0.0"
   :description "Clojure implementation for Luhn algorithm number check"
   :url "https://github.com/blancohugo/luhn"
+  :license {:name "MIT License"
+            :url  "https://spdx.org/licenses/MIT.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]])
